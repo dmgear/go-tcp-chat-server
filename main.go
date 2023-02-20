ef999fe5b82c9f95644e25f6766dad4034a7dc10
Dylan Gearhart <dylanmgearhart@gmail.com>
2023-02-20 03:54:26 -0500

    fixed some bugs  in listenforcommand, added filter String func

--- main.go
+++ main.go
@@ -47,15 +47,30 @@ func (c *Client) giveRole(role string, client *Client) {
 	clientRoles[role] = append(clientRoles[role], client)
 }
 
-func (c *Client) listenForCommand(message string) {
+func isValidRole(role string, roles []string) bool {
+    for _, r := range roles {
+        if r == role {
+            return true
+        }
+    }
+    return false
+}
+
+func filterString(message string) string {
+	message = message[:len(message)-1]
+	return message
+}
+
+func (c *Client) listenForCommand(message string) bool {
 	if !strings.HasPrefix(message, "/") {
-		return
+		return false
 	}
 
 	command := strings.Split(message[1:], " ")
 	switch command[0] {
 	case "join":
-		roomName := strings.TrimSpace(strings.Join(command[1:], " "))
+		roomName := strings.TrimSpace(command[1])
+		roomName = filterString(roomName)
 		room, ok := rooms[roomName]
 		if !ok {
 			room = &Room{
@@ -68,16 +83,35 @@ func (c *Client) listenForCommand(message string) {
 		room.Join(c)
 		joinmsg := "%s has joined"
 		room.broadcastMessage(fmt.Sprintf(joinmsg, c.username), c)
+		return false
 	case "leave":
+	{
 		if room, ok := clientRooms[c]; ok {
 			room.Leave(c)
 			ext := "%s has left"
 			room.broadcastMessage(fmt.Sprintf(ext, c.username), c)
 			c.showRooms()
 		}
+		return false
+	}
+	case "role":
+		if len(command) < 2 {
+			c.conn.Write([]byte("Please specify a role.\n"))
+			return false
+		}
+		role := command[1]
+		if !isValidRole(role, rolesList) {
+			c.conn.Write([]byte("Invalid role.\n"))
+			return false
+		}
+		clientRoles[role] = append(clientRoles[role], c)
+		c.conn.Write([]byte(fmt.Sprintf("You have been assigned the role of %s.\n", role)))
+		return false
+
 	default:
 		// When the command doesn't exist
 		fmt.Println("Unknown command:", command[0])
+		return true
 	}
 }
 
@@ -94,7 +128,7 @@ var static_rooms = []string{"#General", "#Programming", "#Gaming", "#Music", "#M
 func (c *Client) handleConnection() {
 	defer c.conn.Close()
 	c.username = strings.TrimSpace(c.readUsername())
-	c.username = c.username[:len(c.username)-1]
+	c.username = filterString(c.username)
 	fmt.Println("Client", c.username, "connected.")
 
 	c.showRooms()
@@ -109,11 +143,14 @@ func (c *Client) handleConnection() {
 			continue
 		}
 
-	c.listenForCommand(message)
+	listenFor := c.listenForCommand(filterString(message))
+	_= listenFor
 	if room, ok := clientRooms[c]; ok {
 		message = c.username + ": " + message
 		fmt.Println(message)
-		room.broadcastMessage(message, c)
+		if listenFor {
+			room.broadcastMessage(message, c)
+		}
 	}
 	}
 }
@@ -145,9 +182,9 @@ var rooms = make(map[string]*Room)
 var clientRooms = make(map[*Client]*Room)
 var clientRoles = make(map[string][]*Client)
 
+var rolesList = []string{"Programmer", "Gamer", "Rat", "Gopher", "Mod"}
 func createRolesMap() {
-	var roles = []string{"Programmer", "Gamer", "Rat", "Gopher", "Mod"}
-	for _, role := range roles {
+	for _, role := range rolesList {
 		clientRoles[role] = []*Client{}
 	}
 }
