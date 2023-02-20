f1049931193762e854ca1e7a92041e7437ad2a61
TizzyT <tizzyt566@gmail.com>
2023-02-20 04:45:38 -0500

    unfixed formating which fixes it

--- main.go
+++ main.go
@@ -8,8 +8,7 @@ import (
 	"strings"
 )
 
-type Client struct
-{
+type Client struct {
 	conn net.Conn
 	data chan []byte
 	username string
@@ -17,80 +16,64 @@ type Client struct
 	role string
 }
 
-type Room struct
-{
+type Room struct {
 	clients []Client
 	name string
 	Members map[net.Conn]string
 	broadcast chan string
 }
 
-func (r *Room) Join(client *Client)
-{
+func (r *Room) Join(client *Client) {
 	r.Members[client.conn] = client.username
 	clientRooms[client] = r
 }
 
-func (r * Room) Leave(client *Client)
-{
+func (r * Room) Leave(client *Client) {
 	delete(r.Members, client.conn)
 	delete(clientRooms, client)
 }
 
-func (r * Room) broadcastMessage(message string, origin *Client)
-{
-	for conn := range r.Members
-	{
-		if conn == origin.conn
-		{
+func (r * Room) broadcastMessage(message string, origin *Client) {
+	for conn := range r.Members {
+		if conn == origin.conn {
 			continue
 		}
 		conn.Write([]byte(fmt.Sprintf("%s", message)))
 	}
 }
 
-func (c *Client) giveRole(role string, client *Client)
-{
+func (c *Client) giveRole(role string, client *Client) {
 	c.role = role
 	clientRoles[role] = append(clientRoles[role], client)
 }
 
-func isValidRole(role string, roles []string) bool
-{
-    for _, r := range roles
-	{
-        if r == role
-		{
+func isValidRole(role string, roles []string) bool {
+    for _, r := range roles {
+        if r == role {
             return true
         }
     }
     return false
 }
 
-func filterString(message string) string
-{
+func filterString(message string) string {
 	message = message[:len(message)-1]
 	return message
 }
 
-func (c *Client) listenForCommand(message string) bool
-{
-	if !strings.HasPrefix(message, "/")
-	{
+func (c *Client) listenForCommand(message string) bool {
+	if !strings.HasPrefix(message, "/") {
 		return false
 	}
 
 	command := strings.Split(message[1:], " ")
-	switch command[0]
-	{
+	switch command[0] {
 	case "join":
 		roomName := strings.TrimSpace(command[1])
 		roomName = filterString(roomName)
 		room, ok := rooms[roomName]
-		if !ok
-		{
-			room = &Room
-			{
+		if !ok {
+			room = &Room {
 				name:      roomName,
 				Members:   make(map[net.Conn]string),
 				broadcast: make(chan string),
@@ -101,10 +84,8 @@ func (c *Client) listenForCommand(message string) bool
 		joinmsg := "%s has joined"
 		room.broadcastMessage(fmt.Sprintf(joinmsg, c.username), c)
 		return false
-	case "leave":
-	{
-		if room, ok := clientRooms[c]; ok
-		{
+	case "leave": {
+		if room, ok := clientRooms[c]; ok {
 			room.Leave(c)
 			ext := "%s has left"
 			room.broadcastMessage(fmt.Sprintf(ext, c.username), c)
@@ -113,14 +94,12 @@ func (c *Client) listenForCommand(message string) bool
 		return false
 	}
 	case "role":
-		if len(command) < 2 
-		{
+		if len(command) < 2 {
 			c.conn.Write([]byte("Please specify a role.\n"))
 			return false
 		}
 		role := command[1]
-		if !isValidRole(role, rolesList)
-		{
+		if !isValidRole(role, rolesList) {
 			c.conn.Write([]byte("Invalid role.\n"))
 			return false
 		}
@@ -134,20 +113,17 @@ func (c *Client) listenForCommand(message string) bool
 	}
 }
 
-func (c *Client) showRooms()
-{
+func (c *Client) showRooms() {
 	c.conn.Write([]byte("Available rooms:\n"))
 
-	for _, name := range static_rooms
-	{ 
+	for _, name := range static_rooms { 
 		c.conn.Write([]byte("-" + fmt.Sprint(name) + "\n"))
 	}
 }
 
 var static_rooms = []string{"#General", "#Programming", "#Gaming", "#Music", "#Misc", "#The Ratway", "#File transfer"}
 
-func (c *Client) handleConnection()
-{
+func (c *Client) handleConnection() {
 	defer c.conn.Close()
 	c.username = strings.TrimSpace(c.readUsername())
 	c.username = filterString(c.username)
@@ -155,51 +131,41 @@ func (c *Client) handleConnection()
 
 	c.showRooms()
 
-	for 
-	{
+	for {
 		message, err := bufio.NewReader(c.conn).ReadString('\x00')
-		if err != nil
-		{
+		if err != nil {
 			fmt.Println("Client", c.username, "disconnected.")
 			return
 		}
-		if message == ""
-		{
+		if message == "" {
 			continue
 		}
 
 		listenFor := c.listenForCommand(filterString(message))
 		_= listenFor
-		if room, ok := clientRooms[c]; ok
-		{
+		if room, ok := clientRooms[c]; ok {
 			message = c.username + ": " + message
 			fmt.Println(message)
-			if listenFor
-			{
+			if listenFor {
 				room.broadcastMessage(message, c)
 			}
 		}
 	}
 }
 
-func (c *Client) readUsername() string
-{
+func (c *Client) readUsername() string {
     c.conn.Write([]byte("Enter username: "))
     username, _ := bufio.NewReader(c.conn).ReadString('\x00')
     username = strings.TrimSpace(username)
-    if username == ""
-	{
+    if username == "" {
         username = "anon"
     }
     return username
 }
 
-func createRooms()
-{
-	for _, roomName := range static_rooms
-	{
-		room := &Room
-		{
+func createRooms() {
+	for _, roomName := range static_rooms {
+		room := &Room {
 			name : roomName,
 			clients: []Client{},
 			Members: make(map[net.Conn]string),
@@ -215,22 +181,18 @@ var clientRooms = make(map[*Client]*Room)
 var clientRoles = make(map[string][]*Client)
 
 var rolesList = []string{"Programmer", "Gamer", "Rat", "Gopher", "Mod"}
-func createRolesMap()
-{
-	for _, role := range rolesList
-	{
+func createRolesMap() {
+	for _, role := range rolesList {
 		clientRoles[role] = []*Client{}
 	}
 }
 
-func main()
-{
+func main() {
 	createRooms()
 
 	l, err := net.Listen("tcp", "0.0.0.0:1491")
 
-	if err != nil
-	{
+	if err != nil {
 		log.Fatal(err)
 		return
 	}
@@ -239,8 +201,7 @@ func main()
 
 	fmt.Println("Waiting for Daenerys to finish saying all her titles..")
 
-	for
-	{
+	for {
 		conn, err := l.Accept()
 		if err != nil
 		{
